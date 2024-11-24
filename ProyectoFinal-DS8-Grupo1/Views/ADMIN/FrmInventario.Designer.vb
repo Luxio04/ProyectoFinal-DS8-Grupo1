b0112b8e2c3a604e155f4c88b80d2a94956000c4
Luciano Batista <111025097+Luxio04@users.noreply.github.com>
2024-11-24 17:54:37 -0500

    clase y form inventario
    se sube la clase inventario y el form inventario

--- ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmInventario.Designer.vb
+++ ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmInventario.Designer.vb
@@ -22,9 +22,334 @@ Partial Class FrmInventario
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.txtNombreProducto = New System.Windows.Forms.TextBox()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CategoriaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColaboradorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PedidoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProveedorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VolverToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.dtpIngreso = New System.Windows.Forms.DateTimePicker()
+        Me.dgvInventario = New System.Windows.Forms.DataGridView()
+        Me.cbxProveedor = New System.Windows.Forms.ComboBox()
+        Me.btnAgregar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.cbxCategoria = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.dgvInventario, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(154, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre del Producto"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(337, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Descripcion"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(482, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Precio"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(615, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Stock"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(37, 97)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(90, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Fecha de Ingreso"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(420, 97)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(56, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Proveedor"
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(40, 52)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(53, 20)
+        Me.txtId.TabIndex = 8
+        Me.txtId.Visible = False
+        '
+        'txtNombreProducto
+        '
+        Me.txtNombreProducto.Location = New System.Drawing.Point(157, 51)
+        Me.txtNombreProducto.Name = "txtNombreProducto"
+        Me.txtNombreProducto.Size = New System.Drawing.Size(154, 20)
+        Me.txtNombreProducto.TabIndex = 9
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(340, 51)
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(100, 20)
+        Me.txtDescripcion.TabIndex = 10
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(485, 51)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 11
+        '
+        'txtStock
+        '
+        Me.txtStock.Location = New System.Drawing.Point(618, 52)
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.Size = New System.Drawing.Size(100, 20)
+        Me.txtStock.TabIndex = 12
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.VolverToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "Volver"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CategoriaToolStripMenuItem, Me.ColaboradorToolStripMenuItem, Me.PedidoToolStripMenuItem, Me.ProveedorToolStripMenuItem, Me.ReportesToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'CategoriaToolStripMenuItem
+        '
+        Me.CategoriaToolStripMenuItem.Name = "CategoriaToolStripMenuItem"
+        Me.CategoriaToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.CategoriaToolStripMenuItem.Text = "Categoria"
+        '
+        'ColaboradorToolStripMenuItem
+        '
+        Me.ColaboradorToolStripMenuItem.Name = "ColaboradorToolStripMenuItem"
+        Me.ColaboradorToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.ColaboradorToolStripMenuItem.Text = "Colaborador"
+        '
+        'PedidoToolStripMenuItem
+        '
+        Me.PedidoToolStripMenuItem.Name = "PedidoToolStripMenuItem"
+        Me.PedidoToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.PedidoToolStripMenuItem.Text = "Pedido"
+        '
+        'ProveedorToolStripMenuItem
+        '
+        Me.ProveedorToolStripMenuItem.Name = "ProveedorToolStripMenuItem"
+        Me.ProveedorToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.ProveedorToolStripMenuItem.Text = "Proveedor"
+        '
+        'ReportesToolStripMenuItem
+        '
+        Me.ReportesToolStripMenuItem.Name = "ReportesToolStripMenuItem"
+        Me.ReportesToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.ReportesToolStripMenuItem.Text = "Reportes"
+        '
+        'VolverToolStripMenuItem
+        '
+        Me.VolverToolStripMenuItem.Name = "VolverToolStripMenuItem"
+        Me.VolverToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.VolverToolStripMenuItem.Text = "Volver"
+        '
+        'dtpIngreso
+        '
+        Me.dtpIngreso.Location = New System.Drawing.Point(40, 120)
+        Me.dtpIngreso.Name = "dtpIngreso"
+        Me.dtpIngreso.Size = New System.Drawing.Size(200, 20)
+        Me.dtpIngreso.TabIndex = 14
+        '
+        'dgvInventario
+        '
+        Me.dgvInventario.BackgroundColor = System.Drawing.Color.Teal
+        Me.dgvInventario.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvInventario.GridColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.dgvInventario.Location = New System.Drawing.Point(12, 221)
+        Me.dgvInventario.Name = "dgvInventario"
+        Me.dgvInventario.Size = New System.Drawing.Size(748, 217)
+        Me.dgvInventario.TabIndex = 15
+        '
+        'cbxProveedor
+        '
+        Me.cbxProveedor.FormattingEnabled = True
+        Me.cbxProveedor.Location = New System.Drawing.Point(423, 123)
+        Me.cbxProveedor.Name = "cbxProveedor"
+        Me.cbxProveedor.Size = New System.Drawing.Size(153, 21)
+        Me.cbxProveedor.TabIndex = 16
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.Location = New System.Drawing.Point(13, 177)
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregar.TabIndex = 18
+        Me.btnAgregar.Text = "Agregar"
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(136, 177)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnModificar.TabIndex = 19
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(376, 177)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnMostrar.TabIndex = 20
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(270, 177)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 21
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(276, 97)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(52, 13)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "Categoria"
+        '
+        'cbxCategoria
+        '
+        Me.cbxCategoria.FormattingEnabled = True
+        Me.cbxCategoria.Location = New System.Drawing.Point(279, 123)
+        Me.cbxCategoria.Name = "cbxCategoria"
+        Me.cbxCategoria.Size = New System.Drawing.Size(121, 21)
+        Me.cbxCategoria.TabIndex = 24
+        '
+        'FrmInventario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cbxCategoria)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnAgregar)
+        Me.Controls.Add(Me.cbxProveedor)
+        Me.Controls.Add(Me.dgvInventario)
+        Me.Controls.Add(Me.dtpIngreso)
+        Me.Controls.Add(Me.txtStock)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtDescripcion)
+        Me.Controls.Add(Me.txtNombreProducto)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FrmInventario"
         Me.Text = "FrmInventario"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.dgvInventario, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents txtNombreProducto As TextBox
+    Friend WithEvents txtDescripcion As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtStock As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CategoriaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColaboradorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PedidoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProveedorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VolverToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents dtpIngreso As DateTimePicker
+    Friend WithEvents dgvInventario As DataGridView
+    Friend WithEvents cbxProveedor As ComboBox
+    Friend WithEvents btnAgregar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnMostrar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents cbxCategoria As ComboBox
 End Class
--- ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmInventario.vb
+++ ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmInventario.vb
@@ -1,3 +1,235 @@
 ﻿Public Class FrmInventario
+    Private Sub VolverToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VolverToolStripMenuItem.Click
+        Me.Hide()
+        FrmAdmin.Show()
+    End Sub
+
+    Private Sub CategoriaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CategoriaToolStripMenuItem.Click
+        Me.Hide()
+        FrmCategoria.Show()
+    End Sub
+
+    Private Sub ColaboradorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColaboradorToolStripMenuItem.Click
+        Me.Hide()
+        FrmCategoria.Show()
+    End Sub
+
+    Private Sub PedidoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PedidoToolStripMenuItem.Click
+        Me.Hide()
+        FrmPedidos.Show()
+    End Sub
+
+    Private Sub ProveedorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProveedorToolStripMenuItem.Click
+        Me.Hide()
+        FrmProveedor.Show()
+    End Sub
+
+    Private Sub ReportesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReportesToolStripMenuItem.Click
+        Me.Hide()
+        FrmReportes.Show()
+    End Sub
+
+
+    Private Sub btnAgregar_Click(sender As Object, e As EventArgs) Handles btnAgregar.Click
+        Dim objConsulta As New clsInventario
+        Dim mensaje As String
+
+        ' Limpiar espacios en blanco
+        txtNombreProducto.Text = txtNombreProducto.Text.Trim()
+        txtPrecio.Text = txtPrecio.Text.Trim()
+        txtStock.Text = txtStock.Text.Trim()
+        txtDescripcion.Text = txtDescripcion.Text.Trim()
+
+        If Not ValidarCampos() Then
+            Return
+        End If
+
+        Dim categoriaID As Integer = CInt(cbxCategoria.SelectedValue)
+        Dim proveedorID As Integer = CInt(cbxProveedor.SelectedValue)
+        Dim fechaIngreso As Date = dtpIngreso.Value
+
+        ' Verificar si ya existe un proveedor con los mismos datos
+        If Not objConsulta.ValidarInsercionProducto(txtNombreProducto.Text, txtPrecio.Text, proveedorID) Then
+            Return
+        End If
+
+        mensaje = objConsulta.InsertarProducto(txtNombreProducto.Text, txtDescripcion.Text, txtPrecio.Text, txtStock.Text, fechaIngreso, categoriaID, proveedorID)
+        MessageBox.Show(mensaje)
+
+        ' Limpiar los campos
+        LimpiarCampos()
+    End Sub
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Dim objConsulta As New clsInventario
+        Dim mensaje As String
+
+        ' Validar los datos ingresados
+        If String.IsNullOrWhiteSpace(txtId.Text) Then
+            MessageBox.Show("Seleccione un producto para modificar.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Limpiar espacios en blanco
+        txtNombreProducto.Text = txtNombreProducto.Text.Trim()
+        txtDescripcion.Text = txtDescripcion.Text.Trim()
+        txtPrecio.Text = txtPrecio.Text.Trim()
+        txtStock.Text = txtStock.Text.Trim()
+
+        If Not ValidarCampos() Then
+            Return
+        End If
+
+        Dim productoId As Integer = CInt(txtId.Text)
+        Dim nombreProducto As String = txtNombreProducto.Text
+        Dim descripcion As String = txtDescripcion.Text
+        Dim precio As Decimal = Convert.ToDecimal(txtPrecio.Text)
+        Dim Stock As Integer = CInt(txtStock.Text)
+        Dim fechaIngreso As Date = dtpIngreso.Value
+        Dim categoriaID As Integer = CInt(cbxCategoria.SelectedValue)
+        Dim proveedorID As Integer = CInt(cbxProveedor.SelectedValue)
+
+
+        ' Verificar si ya existe un producto con los mismos datos
+        If Not objConsulta.ValidarActualizacionProducto(productoId, nombreProducto, precio, proveedorID) Then
+            Return
+        End If
+
+        mensaje = objConsulta.ActualizarProducto(productoId, nombreProducto, descripcion, precio, Stock, fechaIngreso, proveedorID, categoriaID)
+        MessageBox.Show(mensaje)
+
+        ' Actualiza el DataGridView
+        leerinventario()
+
+        ' Limpiar los campos
+        LimpiarCampos()
+    End Sub
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim objConsulta As New clsInventario
+        Dim mensaje As String
+
+        ' Verificar que un producto haya sido seleccionado
+        If dgvInventario.SelectedRows.Count = 0 Then
+            MessageBox.Show("Por favor, seleccione un producto para eliminar.")
+            Return
+        End If
+
+        ' Obtener el ProductoID desde el DataGridView o el control donde se muestre la lista de productos
+        Dim productoID As Integer = CInt(dgvInventario.SelectedRows(0).Cells("ProductoID").Value)
+
+        ' Confirmar la eliminación
+        Dim result As DialogResult = MessageBox.Show("¿Está seguro que desea eliminar este producto?", "Confirmar eliminación", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            ' Llamar al método de la clase clsInventario para eliminar el producto
+            mensaje = objConsulta.EliminarProducto(productoID)
+            MessageBox.Show(mensaje)
+        End If
+    End Sub
+
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Dim objConsulta As New clsInventario
+        Dim table As New DataTable
+        table = objConsulta.LeerProductos()
+
+        dgvInventario.DataSource = table
+    End Sub
+
+    Private Sub FrmInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LlenarComboBoxProveedores()
+        LlenarComboBoxCategorias()
+
+        dtpIngreso.Format = DateTimePickerFormat.Custom
+        dtpIngreso.CustomFormat = "dd/MM/yyyy" ' Formato de fecha
+        dtpIngreso.Value = DateTime.Now ' Establecer la fecha actual
+        dtpIngreso.ShowUpDown = True
+    End Sub
+    Private Sub LlenarComboBoxProveedores()
+        Dim objProductos As New clsInventario
+        Dim tblProveedor As New DataTable
+
+        tblProveedor = objProductos.ObtenerProveedores()
+        cbxProveedor.DataSource = tblProveedor
+        cbxProveedor.DisplayMember = "NombreProveedor"
+        cbxProveedor.ValueMember = "ProveedorID"
+    End Sub
+
+    Private Sub LlenarComboBoxCategorias()
+        Dim objCategoria As New clsInventario
+        Dim tblCategorias As New DataTable
+
+        tblCategorias = objCategoria.ObtenerCategorias()
+        cbxCategoria.DataSource = tblCategorias
+        cbxCategoria.ValueMember = "CategoriaID"
+        cbxCategoria.DisplayMember = "NombreCategoria"
+    End Sub
+    Private Sub LimpiarCampos()
+        txtId.Clear()
+        txtNombreProducto.Clear()
+        txtDescripcion.Clear()
+        txtPrecio.Clear()
+        txtStock.Clear()
+        cbxCategoria.SelectedIndex = -1
+        cbxProveedor.SelectedIndex = -1
+        dtpIngreso.Value = DateTime.Now
+    End Sub
+
+    Private Function ValidarCampos() As Boolean
+        ' Verificar si alguno de los TextBox está vacío
+        If String.IsNullOrWhiteSpace(txtNombreProducto.Text) Then
+            MessageBox.Show("El campo Nombre es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtNombreProducto.Focus()
+            Return False
+        ElseIf String.IsNullOrWhiteSpace(txtPrecio.Text) Then
+            MessageBox.Show("El campo Precio del Profesor es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtPrecio.Focus()
+            Return False
+        ElseIf String.IsNullOrWhiteSpace(txtStock.Text) Then
+            MessageBox.Show("El campo Stock del Profesor es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtStock.Focus()
+            Return False
+        ElseIf String.IsNullOrWhiteSpace(txtDescripcion.Text) Then
+            MessageBox.Show("El campo Descripcion es obligatorio.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtDescripcion.Focus()
+            Return False
+        ElseIf cbxCategoria.SelectedIndex = -1 Then
+            MessageBox.Show("Debe seleccionar una categoria.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cbxCategoria.Focus()
+            Return False
+        ElseIf cbxProveedor.SelectedIndex = -1 Then
+            MessageBox.Show("Debe seleccionar un proveedor.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cbxProveedor.Focus()
+            Return False
+        End If
+
+        ' Si todos los campos tienen datos válidos
+        Return True
+    End Function
+
+    Public Sub leerInventario()
+        Dim objConsulta As New clsInventario
+        Dim table As New DataTable
+        table = objConsulta.LeerProductos()
+
+        dgvInventario.DataSource = table
+    End Sub
+
+    Private Sub dgvInventario_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvInventario.CellClick
+        ' Verificar que se ha hecho clic en una fila válida
+        If e.RowIndex >= 0 Then
+            ' Obtener la fila seleccionada
+            Dim row As DataGridViewRow = dgvInventario.Rows(e.RowIndex)
+
+            ' Cargar los datos de la fila seleccionada en los TextBoxes
+            txtId.Text = row.Cells("ProductoID").Value.ToString()
+            txtNombreProducto.Text = row.Cells("NombreProducto").Value.ToString()
+            txtDescripcion.Text = row.Cells("descripcion").Value.ToString()
+            txtPrecio.Text = row.Cells("PrecioUnitario").Value.ToString()
+            txtStock.Text = row.Cells("Stock").Value.ToString()
+
+            dtpIngreso.Value = DateTime.Parse(row.Cells("FechaIngreso").Value.ToString())
+
+            cbxCategoria.SelectedValue = row.Cells("CategoriaID").Value
+            cbxProveedor.SelectedValue = row.Cells("ProveedorID").Value
+        End If
+    End Sub
 
 End Class
