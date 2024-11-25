49a784e04e6277e5764fc3636ce3c809fc8037c1
JorgeHitech <jorge_veliz@hitechpanama.com>
2024-11-25 11:07:15 -0500

    reportes avance
    avances de reportes

--- ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmReportes.Designer.vb
+++ ProyectoFinal-DS8-Grupo1/Views/ADMIN/FrmReportes.Designer.vb
@@ -22,9 +22,53 @@ Partial Class FrmReportes
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(219, 143)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar reporte"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(219, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(99, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Generar reporte"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(219, 201)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Generar reporte"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'FrmReportes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "FrmReportes"
         Me.Text = "FrmReportes"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
